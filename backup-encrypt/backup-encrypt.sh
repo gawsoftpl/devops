04641d89c46b417dc3db693f0613f795a7ca5b5a
Pitos <biuro@gawsoft.pl>
2022-08-22 19:41:20 +0200

    Add backup-encrypt

--- backup-encrypt/backup-encrypt.sh
+++ backup-encrypt/backup-encrypt.sh
@@ -3,6 +3,8 @@
 SAVE_DIR=${SAVE_DIR:-'/tmp'}
 date=`date +%Y-%b-%d-%H:%M:%S`
 enc_file=${SAVE_DIR}/backup-${date}.age
+RETENTION_MAX_FILES=${RETENTION_MAX_FILES:-100}
+RETENTION_FILE_MAX_DAYS=${RETENTION_FILE_MAX_DAYS:-90}
 
 if [ ${#FILE} -eq 0 ];
 then
@@ -58,3 +60,25 @@ rm $AWS_SHARED_CREDENTIALS_FILE
 rm $AWS_CONFIG_FILE
 rm $enc_file
 rm $FILE
+
+# Retention
+# Remove old files from s3
+files_limit=${RETENTION_MAX_FILES}
+files_index=0
+aws s3 ls --endpoint-url=https://${S3_ENDPOINT} s3://${S3_BUCKET}/${S3_FOLDER} | tac | while read -r line;  do
+files_index=$(expr $files_index + 1)
+createDate=`echo $line|awk {'print $1" "$2'}`
+createDate=`date -d"$createDate" +%s`
+olderThan=`date -d"-$RETENTION_FILE_MAX_DAYS days" +%s`
+if [ $createDate -lt $olderThan ] && [ $files_index -gt $files_limit ]
+then
+    fileName=`echo $line|awk '{$1=$2=$3=""; print $0}' | sed 's/^[ \t]*//'`
+    echo $fileName
+    if [ $fileName != "" ]
+    then
+        aws s3 rm --endpoint-url=https://${S3_ENDPOINT} "s3://${S3_BUCKET}/${S3_FOLDER}$fileName"
+        echo "COMPLETED S3"
+
+    fi
+fi
+done;
