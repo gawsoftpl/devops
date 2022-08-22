2680ba79c4255e0338f557e582b4627bc75f8a12
Pitos <biuro@gawsoft.pl>
2022-08-22 16:47:51 +0200

    Add backup-encrypt

---
+++ backup-encrypt/backup-encrypt.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+date=`date +%Y-%b-%d-%H:%M:%S`
+enc_file=/tmp/backup-${date}.enc
+
+
+# For ubuntu check that age and awscli is installed
+age --version
+if [[ $? -gt 0 ]];
+then
+  apt install -y age
+fi
+
+aws --version
+if [[ $? -gt 0 ]];
+then
+  apt install -y awscli
+fi
+
+# Encrypt file
+cat $FILE | age -r $BACKUP_ENCRYPTION_AGE_PUBLIC_KEY > $enc_file
+
+export AWS_SHARED_CREDENTIALS_FILE=/tmp/aws-credentials
+export AWS_CONFIG_FILE=/tmp/aws-config
+
+cat <<EOF > $AWS_SHARED_CREDENTIALS_FILE
+[default]
+region=$S3_REGION
+aws_access_key_id=$S3_ACCESS_KEY
+aws_secret_access_key=$S3_SECRET_KEY
+EOF
+
+cat <<EOF > $AWS_CONFIG_FILE
+[default]
+s3 =
+  max_concurrent_requests = 20
+  multipart_chunksize = 64MB
+
+EOF
+
+# Copy to s3
+aws s3 cp --endpoint-url=https://${S3_ENDPOINT} $enc_file s3://${S3_BUCKET}/${S3_FOLDER}
