b5b2167bfffea191faf170043906747fa300edda
Pitos <biuro@gawsoft.pl>
2022-09-28 16:35:08 +0200

    Change backup-encrypt

--- backup-encrypt/backup-encrypt.sh
+++ backup-encrypt/backup-encrypt.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 SAVE_DIR=${SAVE_DIR:-'/tmp'}
 date=`date +%Y-%b-%d-%H:%M:%S`
 enc_file=${SAVE_DIR}/backup-${date}.age
@@ -19,67 +18,60 @@ then
 fi
 
 
-# For ubuntu check that age and awscli is installed
+# Check that required tools is installed
 age --version
 if [[ $? -gt 0 ]];
 then
-  apt install -y age
+  wget -O /tmp/age.tar.gz https://github.com/FiloSottile/age/releases/download/v1.0.0/age-v1.0.0-linux-amd64.tar.gz
+  tar -xvf /tmp/age.tar.gz
+  chmod +x /tmp/age/age
+  cp /tmp/age/age /usr/bin
 fi
 
-aws --version
+s5cmd --version
 if [[ $? -gt 0 ]];
 then
-  apt install -y awscli
+  wget -O /tmp/s5cmd.tar.gz https://github.com/peak/s5cmd/releases/download/v2.0.0/s5cmd_2.0.0_Linux-64bit.tar.gz
+  tar -xvf /tmp/s5cmd.tar.gz
+  chmod +x /tmp/s5cmd
+  mv /tmp/s5cmd /usr/bin
 fi
 
 # Encrypt file
-cat $FILE | age -r $BACKUP_ENCRYPTION_AGE_PUBLIC_KEY > $enc_file
-
-export AWS_SHARED_CREDENTIALS_FILE=/tmp/aws-credentials
-export AWS_CONFIG_FILE=/tmp/aws-config
-
-cat <<EOF > $AWS_SHARED_CREDENTIALS_FILE
-[default]
-region=$S3_REGION
-aws_access_key_id=$S3_ACCESS_KEY
-aws_secret_access_key=$S3_SECRET_KEY
-EOF
-
-cat <<EOF > $AWS_CONFIG_FILE
-[default]
-s3 =
-  max_concurrent_requests = 20
-  multipart_chunksize = 64MB
-
-EOF
+cat $FILE | age -r $AGE_PUBLIC_KEY > $enc_file
 
 # Copy to s3
-aws s3 cp --endpoint-url=https://${S3_ENDPOINT} $enc_file s3://${S3_BUCKET}/${S3_FOLDER}
+s5cmd cp $enc_file s3://${S3_BUCKET}/${S3_FOLDER}
 rm $enc_file
 rm $FILE
 
 # Retention
 # Remove old files from s3
-files_limit=${RETENTION_MAX_FILES}
-files_index=0
-aws s3 ls --endpoint-url=https://${S3_ENDPOINT} s3://${S3_BUCKET}/${S3_FOLDER} | tac | while read -r line;  do
-files_index=$(expr $files_index + 1)
-createDate=`echo $line|awk {'print $1" "$2'}`
-createDate=`date -d"$createDate" +%s`
-olderThan=`date -d"-$RETENTION_FILE_MAX_DAYS days" +%s`
-if [ $createDate -lt $olderThan ] && [ $files_index -gt $files_limit ]
+[ if ${#RETENTION_FILE_MAX_DAYS} -gt 0 ];
 then
-    fileName=`echo $line|awk '{$1=$2=$3=""; print $0}' | sed 's/^[ \t]*//'`
-    echo $fileName
-    if [ $fileName != "" ]
-    then
-        aws s3 rm --endpoint-url=https://${S3_ENDPOINT} "s3://${S3_BUCKET}/${S3_FOLDER}$fileName"
-        echo "COMPLETED S3"
+  files_limit=${RETENTION_MAX_FILES}
+  files_index=0
+  is_dir=`echo $line | grep DIR`
+  if [ ${#is_dir} -eq 0 ];
+  then
+    s5cmd ls s3://${S3_BUCKET}/${S3_FOLDER} | tac | while read -r line;  do
+    files_index=$(expr $files_index + 1)
+    createDate=`echo $line|awk {'print $1" "$2'}`
+    if [ ${#createDate} -gt 0 ];
+      createDate=`date -d"$createDate" +%s`
+      olderThan=`date -d"-$RETENTION_FILE_MAX_DAYS days" +%s`
+      if [ $createDate -lt $olderThan ] && [ $files_index -gt $files_limit ]
+      then
+          fileName=`echo $line|awk '{$1=$2=$3=""; print $0}' | sed 's/^[ \t]*//'`
+          echo $fileName
+          if [ $fileName != "" ]
+          then
+              s5cmd rm "s3://${S3_BUCKET}/${S3_FOLDER}${fileName}"
+              echo "$fileName removed"
 
+          fi
+      fi
     fi
+  fi
+  done;
 fi
-done;
-
-
-rm $AWS_SHARED_CREDENTIALS_FILE
-rm $AWS_CONFIG_FILE
